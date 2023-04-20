adf7467ce2e74dbf1584c1d875479bc03346d672
Loveth121 <samuellovethosato@gmail.com>
2023-04-20 08:24:13 -0700

    101

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - A C program that prints with puts function
+
+* Return: 0
+*/
+#include <stdio.h>
+
+int main() {
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
+}
