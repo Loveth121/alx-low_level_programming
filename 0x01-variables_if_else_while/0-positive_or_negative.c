68d84459867c9cb06716389d1906fd9432661058
Loveth121 <samuellovethosato@gmail.com>
2023-04-21 21:21:29 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/* This program will assign a random number to the variable n each time it is executed */
+/**
+ * main - entry point
+ * Discription: chech if number is positive or negative
+ * Return: always 0
+*/
 int main(void)
 {
 	int n;
