d8e79a135bf86506117d5c5eadbca206b9836520
Loveth121 <samuellovethosato@gmail.com>
2023-04-28 12:18:27 -0700

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - causes an infinite loop
+* Description: infinite loop
 * Return: 0
 */
 
@@ -12,12 +13,11 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/**
-* while (i < 10)
-* {
-* putchar(i);
-* }
-*/
+/* while (i < 10) */
+/* { */
+/* putchar(i);*/
+/* } */
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
