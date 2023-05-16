b051134692195408756cc2f8eb55f497a8eed344
Loveth121 <samuellovethosato@gmail.com>
2023-05-16 12:13:04 -0700

    task 4

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all the arguments of the program.
+ * @ac: The argument count.
+ * @av: Array of argument strings.
+ *
+ * Return: Pointer to the concatenated string, or NULL on failure.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, len, total_len = 0;
+	int k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	/* Calculate the total length of the arguments */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			total_len++;
+		total_len++; /* Account for the newline character */
+	}
+
+	str = malloc((total_len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	/* Copy the arguments to the concatenated string */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			str[k++] = av[i][j];
+		str[k++] = '\n'; /* Add the newline character */
+	}
+
+	str[k] = '\0'; /* Add the null terminator */
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - Frees the memory allocated for a 2-dimensional grid.
+ * @grid: Pointer to the grid.
+ * @height: The height of the grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
